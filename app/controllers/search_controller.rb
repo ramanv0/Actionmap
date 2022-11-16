7e10bcafb45295e5d6cb95efee001d20b3e63381
Raman Varma <ramanvarma@berkeley.edu>
2022-11-16 13:46:19 -0800

    remove log comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,8 +6,6 @@ class SearchController < ApplicationController
   def search
     address = params[:address]
     service = Google::Apis::CivicinfoV2::CivicInfoService.new
-    puts("!!!!!!GOOGLE API KEY!!!!!!")
-    puts(Rails.application.credentials[:GOOGLE_API_KEY])
     service.key = Rails.application.credentials[:GOOGLE_API_KEY]
     result = service.representative_info_by_address(address: address)
     @representatives = Representative.civic_api_to_representative_params(result)
