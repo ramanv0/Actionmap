7d54fbf37730e796391ed89cb6497490b815ca3b
Raman Varma <ramanvarma@berkeley.edu>
2022-11-23 01:36:55 -0800

    Fix orphaned migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_23_091234) do
+ActiveRecord::Schema.define(version: 2022_11_23_093248) do
 
   create_table "counties", force: :cascade do |t|
     t.string "name", null: false
